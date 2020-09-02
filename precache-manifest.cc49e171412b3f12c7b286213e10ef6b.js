3495c3c1b0781fae994fcea2818d7f8300edd408
sindhu <chitranivas19@gmail.com>
2020-09-02 10:55:26 +0530

    Updates

---
+++ precache-manifest.cc49e171412b3f12c7b286213e10ef6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12636a8b5bdd1d809d9011e3234de615",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "f558b3a8b832ee9cfecd",
+    "url": "/react-profile/static/css/main.8183868a.chunk.css"
+  },
+  {
+    "revision": "bbc7cbf7424b32b51221",
+    "url": "/react-profile/static/js/2.3069a4fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-profile/static/js/2.3069a4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f558b3a8b832ee9cfecd",
+    "url": "/react-profile/static/js/main.960c18e2.chunk.js"
+  },
+  {
+    "revision": "a74323b8ed38afce2a5a",
+    "url": "/react-profile/static/js/runtime-main.5dffbbbe.js"
+  }
+]);
